fda0433191b944f28c61986cd559593a62b91515
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-01-19 21:57:43 +0100

    countdown update

--- Pokezon/index.php
+++ Pokezon/index.php
@@ -22,6 +22,7 @@
                             Lista item
                         </a>
                     </li>
+                    <div data-type="countdown" data-id="3012079" class="tickcounter" style="width: 100%; position: relative; padding-bottom: 25%"><a href="//www.tickcounter.com/countdown/3012079/la-mirri-che-ci-da-30l" title="la mirri che ci da 30L">la mirri che ci da 30L</a><a href="//www.tickcounter.com/" title="Countdown">Countdown</a></div><script>(function(d, s, id) { var js, pjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//www.tickcounter.com/static/js/loader.js"; pjs.parentNode.insertBefore(js, pjs); }(document, "script", "tickcounter-sdk"));</script>
                 </ul> 
             </h2>
         </header>
