5ecb794a79c48adf96a24961b0d76245a4e482ef
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-01-21 22:56:38 +0100

    add itemCategories query

--- Pokezon/db/database.php
+++ Pokezon/db/database.php
@@ -22,5 +22,19 @@ class DatabaseHelper{
                 $result = $stmt->get_result();
                 return $result->fetch_all(MYSQLI_ASSOC);
             }
+            
+            public function pokeItemCategoriesGet(){
+                $stmt = $this->db->prepare("
+                    SELECT identifier FROM `item_categories`
+                    where  identifier = 'standard-balls'
+                    or identifier = 'special-balls'
+                    or identifier = 'all-machines'
+                    or identifier = 'evolution'
+                    or identifier = 'medicine';
+                ");
+                $stmt->execute();
+                $result = $stmt->get_result();
+                return $result->fetch_all(MYSQLI_ASSOC);
+            }
         }
 ?>
