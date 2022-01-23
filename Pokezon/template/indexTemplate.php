198f41b564240ddf5c4b57e0171695f86053bb92
Michele Nardini <michele.nardini3g@gmail.com>
2022-01-23 20:15:30 +0100

    update funzionante

--- Pokezon/template/indexTemplate.php
+++ Pokezon/template/indexTemplate.php
@@ -11,56 +11,6 @@
 	</script>
 </head>
 <body>
-    <nav class="navbar">
-        <a class="logo" href="index.php">
-            <img src="../resources/logo.png" width="187.5" height="150"alt="">
-        </a>
-        <a class="logoRestrinct" href="index.php">
-            <img src="../resources/logoRestrinct.png" width="396,91" height="50"alt="">
-        </a>
-        <button class="hb-button" id="hb-btn">
-            <img src="../resources/navbtn.png" width="60" height="60" alt="">
-        </button>
-        <ul class="nav-container">
-            <a href="index.php">
-                <li class="nav-home">
-                    <div>
-                        <img src="../resources/pokeball.png" width="50" height="50" alt="">
-                    </div>
-                    <div>
-                        <p>Home</p>
-                    </div>
-                </li>
-            </a>
-            <a href="tablePokemon.php">
-                <li class="nav-poke">
-                    <div>
-                        <img src="../resources/pokemon.jpg" width="50" height="50" alt="">
-                    </div>
-                    <div>
-                        <p>Pokemon</p>
-                    </div>
-                </li>
-            </a>
-            <a href="tableItem.php">
-                <li class="nav-item">
-                <div>
-                        <img src="../resources/item.png" width="50" height="50" alt="">
-                    </div>
-                    <div>
-                        <p>Item</p>
-                    </div>
-                </li>
-            </a>
-        </ul>
-        <button class="login-button" id="login-btn">
-            <img src="../resources/login1.png" width="75" height="75" alt="">
-        </button>
-        <button class="shop-button" id="shop-btn">
-            <img src="../resources/shopping.png" width="75" height="75" alt="">
-        </button>
-    </nav>
-    <script src="navScript.js"></script>
     <main>
              <!-- Slideshow container -->
 
