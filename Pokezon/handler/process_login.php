56d28d56dbb1bc6c280963cc345f0f43dac34140
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-01-28 00:04:01 +0100

    utente setted wjth get

--- Pokezon/handler/process_login.php
+++ Pokezon/handler/process_login.php
@@ -1,20 +1,20 @@
 <?php 
-
-
 include 'session.php';
-
 sec_session_start(); // usiamo la nostra funzione per avviare una sessione php sicura
 
 if(isset($_POST['userName'], $_POST['p'])) { 
-   $email = $_POST['userName'];
+   $username= $_POST['userName'];
    $password = $_POST['p']; // Recupero la password criptata.
     $dbh = new mysqli("localhost", "root", "", "pokedb", 3360); 
                 if($dbh->connect_error){
                     die("Connesione fallita al db");
                 }
-   if(login($email, $password, $dbh) == true) {
-      // Login eseguito
+   if(login($username, $password, $dbh) == true) {
+    // Login eseguito
       echo 'Success: You have been logged in!';
+
+      header("Location: https://localhost/Progetto-Tecnologie-Web/Pokezon/index.php?utente=".$username); // TODO usare template params
+      exit();
    } else {
       // Login fallito
       header('Location: ./login.php?error=1');
@@ -23,6 +23,4 @@ if(isset($_POST['userName'], $_POST['p'])) {
    // Le variabili corrette non sono state inviate a questa pagina dal metodo POST.
    echo 'Invalid Request';
 }
-
-
 ?>
