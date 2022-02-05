09b2bf7c5a4ed7deb38ee3e555748a3490171637
『 Matteo Santoro 』 <megadigimatt@gmail.com>
2022-02-05 19:46:07 +0100

    back

--- Pokezon/template/loginTemplate.php
+++ Pokezon/template/loginTemplate.php
@@ -38,8 +38,25 @@
                     <span class="fas fa-key"></span> 
                     <input type="password" name="p" id="pwd" placeholder="Password" aria-label="password placeholder"> <!-- aria label is for accessability form -->
                 </div>
-                    <input class="btn mt-3" type="button" value="Login"  aria-label="login button" onclick="formhash(this.form, this.form.pwd);"> <!-- aria label is for accessability form -->
+                    <input id="login" class="btn mt-3" type="button" value="Login"  aria-label="login button" onclick="formhash(this.form, this.form.pwd);"> <!-- aria label is for accessability form -->
             </form>
+                  <script>
+                        // Get the input field
+
+                        var input = document.getElementById("login");
+
+                        // Execute a function when the user releases a key on the keyboard
+                        input.addEventListener("keyup", function(event) {
+                            console.log("heiiii")
+                          // Number 13 is the "Enter" key on the keyboard
+                          if (event.keyCode === 13) {
+                            // Cancel the default action, if needed
+                            event.preventDefault();
+                            // Trigger the button element with a click
+                            document.getElementById("myBtn").click();
+                          }
+                        }); 
+                    </script>
                 <div>
                     <br>
                     <p>
